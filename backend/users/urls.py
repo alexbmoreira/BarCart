d435306b9a070547d6d0b46e2f8bf6a3f4c4086f
alexbmoreira <alexbmoreira@gmail.com>
2021-03-11 13:51:25 -0500

    Can like a drink

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import ProfileDetailView
+from .views import ProfileDetailView, DrinkLikeView
 
 profile_detail = ProfileDetailView.as_view()
+drink_like = DrinkLikeView.as_view()
 
 urlpatterns = [
     path("profiles/<int:user_id>/", profile_detail, name="profile_detail"),
+    path("likes/", drink_like, name="drink_like"),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .serializers import ProfileSerializer
+from .serializers import ProfileSerializer, DrinkLikeSerializer
 from .models import Profile
 
 
@@ -15,3 +15,19 @@ class ProfileDetailView(APIView):
         profile = get_object_or_404(Profile, user__id=user_id)
         serializer = ProfileSerializer(profile)
         return Response(serializer.data)
+
+
+class DrinkLikeView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        like = request.data
+        like['user'] = request.user.id
+        serializer = DrinkLikeSerializer(data=like)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
