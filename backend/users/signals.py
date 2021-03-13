620ef0ce7effc326c4b30756c09be30cf01b59d5
alexbmoreira <alexbmoreira@gmail.com>
2021-03-12 19:24:11 -0500

    On tap list updates on any on hand add or delete

--- backend/users/signals.py
+++ backend/users/signals.py
@@ -20,9 +20,15 @@ def save_user_profile(sender, instance, **kwargs):
 @receiver(post_save, sender=OnHandIngredient)
 def save_on_hand(sender, instance, **kwargs):
     for drink in Profile.objects.get_on_tap(instance.user.profile):
-        OnTapDrink.objects.create(user=instance.user, drink=drink)
+        try:
+            OnTapDrink.objects.create(user=instance.user, drink=drink)
+        except Exception as e:
+            print(e)
+
 
 @receiver(post_delete, sender=OnHandIngredient)
 def delete_on_hand(sender, instance, **kwargs):
-    for drink in Profile.objects.get_on_tap(instance.user.profile):
-        OnTapDrink.objects.create(user=instance.user, drink=drink)
+    for drink in instance.user.ontapdrink_set.all():
+        ingredients = drink.drink.drinkingredient_set.prefetch_related('ingredient')
+        if instance.ingredient in [ing.ingredient for ing in ingredients]:
+            drink.delete()
