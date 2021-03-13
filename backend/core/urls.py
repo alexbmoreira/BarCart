47425916ced926c97f942cc190d24039da0d7c8e
alexbmoreira <alexbmoreira@gmail.com>
2021-03-12 20:19:50 -0500

    Added url endpoint for popularity

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,10 +1,12 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import DrinkListView, DrinkDetailView, IngredientListView, IngredientDetailView
+from .views import (DrinkDetailView, DrinkListView, DrinkPopularView,
+                    IngredientDetailView, IngredientListView)
 
 drink_detail = DrinkDetailView.as_view()
 drink_list = DrinkListView.as_view()
+drink_popular = DrinkPopularView.as_view()
 ingredient_detail = IngredientDetailView.as_view()
 ingredient_list = IngredientListView.as_view()
 
@@ -12,6 +14,7 @@ urlpatterns = [
     path("drinks/", drink_list, name="drink_list"),
     path("drinks/<int:drink_id>/", drink_detail, name="drink_detail"),
     path("drinks/search/<str:search>/", drink_list, name="drink_list_search"),
+    path("drinks/popular/", drink_popular, name="drink_popular"),
     path("ingredients/", ingredient_list, name="ingredient_list"),
     path("ingredients/<int:ingredient_id>/", ingredient_detail, name="ingredient_detail"),
     path("ingredients/search/<str:search>/", ingredient_list, name="ingredient_list_search"),
