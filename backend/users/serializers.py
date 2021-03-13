da142d31578e95ed1ead5caed473f0b9195d1033
alexbmoreira <alexbmoreira@gmail.com>
2021-03-12 19:31:37 -0500

    On tap data is stored for each user

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,7 +1,7 @@
 from core.serializers import DrinkSerializer
 from rest_framework import serializers
 
-from .models import DrinkLike, OnHandIngredient, Profile
+from .models import DrinkLike, OnHandIngredient, OnTapDrink, Profile
 
 
 class DrinkLikeSerializer(serializers.ModelSerializer):
@@ -18,6 +18,13 @@ class OnHandIngredientSerializer(serializers.ModelSerializer):
         fields = ("id", "user", "ingredient")
 
 
+class OnTapDrinkSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = OnTapDrink
+        fields = ("id", "user", "drink")
+
+
 class ProfileSerializer(serializers.ModelSerializer):
     user_id = serializers.ReadOnlyField(source='user.id')
     username = serializers.ReadOnlyField(source='user.username')
@@ -26,28 +33,8 @@ class ProfileSerializer(serializers.ModelSerializer):
     drinks = DrinkSerializer(many=True, source='user.drink_set.all')
     likes = DrinkLikeSerializer(many=True, source='user.drinklike_set.all')
     on_hand = OnHandIngredientSerializer(many=True, source='user.onhandingredient_set.all')
-    on_tap = serializers.SerializerMethodField()
+    on_tap = OnTapDrinkSerializer(many=True, source='user.ontapdrink_set.all')
 
     class Meta:
         model = Profile
         fields = ['user_id', 'username', 'first_name', 'last_name', 'bio', 'drinks', 'likes', 'on_hand', 'on_tap']
-
-    def get_on_tap(self, obj):
-        on_tap = Profile.objects.get_on_tap(obj)
-        return DrinkSerializer(on_tap, many=True).data
-
-        # likes = obj.user.drinklike_set.all().values('drink')
-        # on_hand = obj.user.onhandingredient_set.all().values('ingredient')
-
-        # on_tap = []
-        # for drink in likes:
-        #     add = True
-        #     for ingredient in Drink.objects.get(id=drink['drink']).drinkingredient_set.all().values('ingredient'):
-        #         if ingredient not in on_hand:
-        #             add = False
-        #     if add:
-        #         on_tap.append(drink['drink'])
-
-        # on_tap_q = Drink.objects.filter(id__in=on_tap)
-
-        # return DrinkSerializer(on_tap_q, many=True).data
--- backend/users/signals.py
+++ backend/users/signals.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 
 from .models import OnHandIngredient, OnTapDrink, Profile
