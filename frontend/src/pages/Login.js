a53b32de922fc514d70c75d41d329a210e859d35
alexbmoreira <alexbmoreira@gmail.com>
2021-03-31 18:25:09 -0400

    Add states for uform inputs

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, Text, Button, TextInput } from 'react-native-paper';
 
 export default function Login({ navigation }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <Card>
       <Text>Login</Text>
-      <TextInput placeholder="Username" />
-      <TextInput placeholder="Password" />
+      <TextInput placeholder="Username" value={username} onChangeText={(newUsername) => setUsername(newUsername)} autoCapitalize="none" autoCorrect={false} />
+      <TextInput placeholder="Password" value={password} onChangeText={(newPassword) => setPassword(newPassword)} autoCapitalize="none" autoCorrect={false} secureTextEntry />
       <Button title="Register" onPress={() => navigation.navigate('Register')}>
         <Text>Register</Text>
       </Button>
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, Text, Button, TextInput } from 'react-native-paper';
 
 export default function Register({ navigation }) {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
   return (
     <Card>
       <Text>Register</Text>
-      <TextInput placeholder="First Name" />
-      <TextInput placeholder="Last Name" />
-      <TextInput placeholder="Username" />
-      <TextInput placeholder="Email Address" />
-      <TextInput placeholder="Password" />
-      <TextInput placeholder="Confirm Password" />
+      <TextInput placeholder="First Name" value={firstName} onChangeText={(newFirstName) => setFirstName(newFirstName)} />
+      <TextInput placeholder="Last Name" value={lastName} onChangeText={(newLastName) => setLastName(newLastName)} />
+      <TextInput placeholder="Username" value={username} onChangeText={(newUsername) => setUsername(newUsername)} autoCapitalize="none" autoCorrect={false} />
+      <TextInput placeholder="Email Address" value={email} onChangeText={(newEmail) => setEmail(newEmail)} autoCapitalize="none" autoCorrect={false} />
+      <TextInput placeholder="Password" value={password} onChangeText={(newPassword) => setPassword(newPassword)} autoCapitalize="none" autoCorrect={false} secureTextEntry />
+      <TextInput placeholder="Confirm Password" value={confirmPassword} onChangeText={(newConfirmPassword) => setConfirmPassword(newConfirmPassword)} autoCapitalize="none" autoCorrect={false} secureTextEntry />
       <Button title="Login" onPress={() => navigation.navigate('Login')}>
         <Text>Login</Text>
       </Button>
