b3733fc734a73aa3dc1499b9b57a0e9a350102c2
alexbmoreira <alexbmoreira@gmail.com>
2021-03-31 18:09:42 -0400

    Added login and register fields

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Card, Text, Button } from 'react-native-paper';
+import { Card, Text, Button, TextInput } from 'react-native-paper';
 
 export default function Login({ navigation }) {
   return (
     <Card>
       <Text>Login</Text>
-      <Button title="Next screen" onPress={() => navigation.navigate('Main')}>
-        <Text>Next</Text>
-      </Button>
+      <TextInput placeholder="Username" />
+      <TextInput placeholder="Password" />
       <Button title="Register" onPress={() => navigation.navigate('Register')}>
         <Text>Register</Text>
+      </Button>
+      <Button title="Next screen" onPress={() => navigation.navigate('Main')}>
+        <Text>Next</Text>
       </Button>
     </Card>
   );
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import { Card, Text, Button } from 'react-native-paper';
+import { Card, Text, Button, TextInput } from 'react-native-paper';
 
 export default function Register({ navigation }) {
   return (
     <Card>
       <Text>Register</Text>
-      <Button title="Next screen" onPress={() => navigation.navigate('Main')}>
-        <Text>Next</Text>
-      </Button>
+      <TextInput placeholder="First Name" />
+      <TextInput placeholder="Last Name" />
+      <TextInput placeholder="Username" />
+      <TextInput placeholder="Email Address" />
+      <TextInput placeholder="Password" />
+      <TextInput placeholder="Confirm Password" />
       <Button title="Login" onPress={() => navigation.navigate('Login')}>
         <Text>Login</Text>
+      </Button>
+      <Button title="Next screen" onPress={() => navigation.navigate('Main')}>
+        <Text>Next</Text>
       </Button>
     </Card>
   );
