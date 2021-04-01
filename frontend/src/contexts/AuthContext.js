e19c2bf16430d19f27b63026bea1124d9e5204bd
alexbmoreira <alexbmoreira@gmail.com>
2021-03-31 23:21:29 -0400

    Flesh out AuthContext

--- frontend/src/contexts/AuthContext.js
+++ frontend/src/contexts/AuthContext.js
@@ -7,4 +7,27 @@ const authReducer = (state, action) => {
   }
 };
 
-export const { Provider, Context } = createDataContext(authReducer, {}, { isSignedIn: false });
+const register = (dispatch) => {
+  return ({ firstName, LastName, username, email, password, confirmPassword }) => {
+    // make an API request to sign up
+    // if we sign up: authenticate, change the state. signedIn = true
+    // if not: send back an error
+  };
+};
+
+const login = (dispatch) => {
+  return ({ username, password }) => {
+    // API request
+    // success: authenticate, change the state
+    // error: send back an error
+  };
+};
+
+const logout = (dispatch) => {
+  return () => {
+    // API request
+    // success: sign out
+  };
+};
+
+export const { Provider, Context } = createDataContext(authReducer, { register, login, logout }, { isSignedIn: false });
