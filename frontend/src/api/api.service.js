08f55123e0e4f6120b4483eda631db0f7c0100ae
alexbmoreira <alexbmoreira@gmail.com>
2021-04-01 03:07:05 -0400

    Added environment variables on frontend for ngrok

--- frontend/src/api/api.service.js
+++ frontend/src/api/api.service.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { NGROK_HOST } from '@env';
 
 const instance = axios.create({
-  baseURL: 'http://02eed1458b05.ngrok.io/api',
+  baseURL: `${NGROK_HOST}/api`,
 });
 
 instance.interceptors.request.use(
   async (config) => {
+    console.log(`${NGROK_HOST}`);
     const token = await AsyncStorage.getItem('access_token');
     if (token) {
       config.headers.Authorization = `Authorization ${token}`;
