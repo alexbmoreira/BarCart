31f78c0d39bbfadb42b971070ce3421bdcf7a31a
alexbmoreira <alexbmoreira@gmail.com>
2021-04-01 02:34:12 -0400

    All requests *should* be authenticated once logged in

--- frontend/src/api/api.service.js
+++ frontend/src/api/api.service.js
@@ -1,5 +1,21 @@
 import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default axios.create({
-  baseURL: 'http://a532a7b24db5.ngrok.io/api',
+const instance = axios.create({
+  baseURL: 'http://02eed1458b05.ngrok.io/api',
 });
+
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem('access_token');
+    if (token) {
+      config.headers.Authorization = `Authorization ${token}`;
+    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+export default instance;
