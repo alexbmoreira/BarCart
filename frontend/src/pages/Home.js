1fb3fb9e76f9d0bdec4c1def4125918633624866
alexbmoreira <alexbmoreira@gmail.com>
2021-04-06 13:45:03 -0400

    Got rid of local state

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Text } from 'react-native-paper';
 
@@ -7,28 +7,22 @@ import { Context as DrinkContext } from '../contexts/DrinkContext';
 export default function Home({ navigation }) {
   const { state, getUserDrinks, getUserOnTap } = useContext(DrinkContext);
 
-  const [userDrinks, setUserDrinks] = useState([]);
-  const [userOnTap, setUserOnTap] = useState([]);
-
   useEffect(() => {
     const ud = navigation.addListener('focus', async () => {
       await getUserDrinks();
-      console.log(state);
-      setUserDrinks(state.userDrinks);
 
       await getUserOnTap();
-      setUserOnTap(state.userOnTap);
     });
 
     return ud;
   }, [navigation, getUserDrinks, getUserOnTap, state]);
 
-  const userDrinksArray = userDrinks.map((drink, i) => {
-    return <Text key={userDrinks[i].id}>{userDrinks[i].name}</Text>;
+  const userDrinksArray = state.userDrinks.map((drink, i) => {
+    return <Text key={state.userDrinks[i].id}>{state.userDrinks[i].name}</Text>;
   });
 
-  const userOnTapArray = userOnTap.map((drink, i) => {
-    return <Text key={userOnTap[i].id}>{userOnTap[i].name}</Text>;
+  const userOnTapArray = state.userOnTap.map((drink, i) => {
+    return <Text key={state.userOnTap[i].id}>{state.userOnTap[i].name}</Text>;
   });
 
   return (
