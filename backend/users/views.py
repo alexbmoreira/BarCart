23da64a367aff80c96d0505ffcc19e0bac9b9531
alexbmoreira <alexbmoreira@gmail.com>
2021-04-05 22:30:58 -0400

    Gets for user likes, on hand, on tap

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,4 +1,4 @@
-from core.serializers import DrinkSerializer
+from core.serializers import DrinkSerializer, IngredientSerializer
 from core.models import Drink
 from django.shortcuts import get_object_or_404
 from rest_framework import status
@@ -23,6 +23,11 @@ class ProfileDetailView(APIView):
 class OnHandIngredientView(APIView):
     permission_classes = (IsAuthenticated,)
 
+    def get(self, request):
+        oh_ings = request.user.onhandingredient_set.select_related('ingredient')
+        serializer = IngredientSerializer([oh_ing.ingredient for oh_ing in oh_ings], many=True)
+        return Response(serializer.data)
+
     def post(self, request):
         ingredients = [dict(ingredient, **{'user': request.user.id}) for ingredient in request.data]
         serializer = OnHandIngredientSerializer(data=ingredients, many=True)
@@ -43,6 +48,11 @@ class OnHandIngredientView(APIView):
 class DrinkLikeView(APIView):
     permission_classes = (IsAuthenticated,)
 
+    def get(self, request):
+        drink_likes = request.user.drinklike_set.select_related('drink')
+        serializer = DrinkSerializer([drink.drink for drink in drink_likes], many=True)
+        return Response(serializer.data)
+
     def post(self, request):
         like = request.data
         like['user'] = request.user.id
@@ -58,6 +68,15 @@ class DrinkLikeView(APIView):
         unlike = get_object_or_404(DrinkLike, drink__id=request.data['drink'], user=request.user)
         unlike.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class OnTapDrinkView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        on_tap = request.user.ontapdrink_set.select_related('drink')
+        serializer = DrinkSerializer([drink.drink for drink in on_tap], many=True)
+        return Response(serializer.data)
 
 
 class UserDrinksView(APIView):
