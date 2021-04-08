dbd55118d71d7df3b98bb241c68da33bbdde2fd2
alexbmoreira <alexbmoreira@gmail.com>
2021-04-08 12:09:03 -0400

    Put profile settings in scroll view

--- frontend/src/pages/ProfileSettings.js
+++ frontend/src/pages/ProfileSettings.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { StyleSheet } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 import { Text, Title, Button, TextInput, withTheme } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
@@ -16,31 +17,35 @@ function ProfileSettings() {
 
   return (
     <SafeAreaView>
-      <Spacer>
-        <Button onPress={logoutUser}>Log out</Button>
-        <Title>[USERNAME]</Title>
-        <Text style={styles.text}>account settings</Text>
-      </Spacer>
-      <Spacer x>
-        <Title>Your on-hand ingredients</Title>
-        <TextInput mode={'outlined'} disabled={true} placeholder={'Ingredients'} value={''} />
-        <Button style={styles.button} mode={'contained'}>
-          Edit Ingredients
-        </Button>
-      </Spacer>
-      <Spacer x>
-        <Title>Username</Title>
-        <TextInput mode={'outlined'} disabled={true} placeholder={'Username'} value={''} />
-      </Spacer>
-      <Spacer x>
-        <Title>Password</Title>
-        <TextInput mode={'outlined'} disabled={true} placeholder={'Password'} value={''} />
-      </Spacer>
-      <Spacer x>
-        <Button style={styles.button} mode={'contained'}>
-          Edit Log-In Credentials
-        </Button>
-      </Spacer>
+      <ScrollView>
+        <Spacer y amount={5}>
+          <Spacer x amount={15}>
+            <Button onPress={logoutUser}>Log out</Button>
+            <Title>[USERNAME]</Title>
+            <Text style={styles.text}>account settings</Text>
+          </Spacer>
+        </Spacer>
+        <Spacer x>
+          <Title>Your on-hand ingredients</Title>
+          <TextInput mode={'outlined'} disabled={true} placeholder={'Ingredients'} value={''} />
+          <Button style={styles.button} mode={'contained'}>
+            Edit Ingredients
+          </Button>
+        </Spacer>
+        <Spacer x>
+          <Title>Username</Title>
+          <TextInput mode={'outlined'} disabled={true} placeholder={'Username'} value={''} />
+        </Spacer>
+        <Spacer x>
+          <Title>Password</Title>
+          <TextInput mode={'outlined'} disabled={true} placeholder={'Password'} value={''} />
+        </Spacer>
+        <Spacer x>
+          <Button style={styles.button} mode={'contained'}>
+            Edit Log-In Credentials
+          </Button>
+        </Spacer>
+      </ScrollView>
     </SafeAreaView>
   );
 }
