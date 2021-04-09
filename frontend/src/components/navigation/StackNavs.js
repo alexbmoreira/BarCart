6bcfc172ce168c9a15b657dc310fac18ee7bb41a
alexbmoreira <alexbmoreira@gmail.com>
2021-04-09 17:42:34 -0400

    Styled headers

--- frontend/src/components/navigation/StackNavs.js
+++ frontend/src/components/navigation/StackNavs.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import { useTheme } from 'react-native-paper';
 import { FontAwesome } from '@expo/vector-icons';
 
 import DrinkDetail from '../../pages/DrinkDetail';
@@ -14,17 +13,30 @@ import ProfileSettings from '../../pages/ProfileSettings';
 
 import Spacer from '../theme/Spacer';
 
+import BarCartTheme from '../../BarCartTheme';
+
 const HomeNav = createStackNavigator();
 const SearchNav = createStackNavigator();
 const CreateDrinkNav = createStackNavigator();
 const OnTapNav = createStackNavigator();
 const ProfileNav = createStackNavigator();
 
+const colors = BarCartTheme.theme.colors;
+
+const headerOptions = {
+  headerStyle: {
+    backgroundColor: colors.primary,
+  },
+  headerTitleStyle: {
+    color: colors.background,
+  },
+};
+
 function HomeStack() {
   return (
     <HomeNav.Navigator>
-      <HomeNav.Screen name="Home" component={Home} options={{ headerLeft: () => null }} />
-      <HomeNav.Screen name="DrinkDetail" component={DrinkDetail} />
+      <HomeNav.Screen name="Home" component={Home} options={{ ...headerOptions, headerLeft: () => null }} />
+      <HomeNav.Screen name="DrinkDetail" component={DrinkDetail} options={headerOptions} />
     </HomeNav.Navigator>
   );
 }
@@ -32,8 +44,8 @@ function HomeStack() {
 function SearchStack() {
   return (
     <SearchNav.Navigator>
-      <SearchNav.Screen name="Search" component={Search} options={{ headerLeft: () => null }} />
-      <SearchNav.Screen name="DrinkDetail" component={DrinkDetail} />
+      <SearchNav.Screen name="Search" component={Search} options={{ ...headerOptions, headerLeft: () => null }} />
+      <SearchNav.Screen name="DrinkDetail" component={DrinkDetail} options={headerOptions} />
     </SearchNav.Navigator>
   );
 }
@@ -41,7 +53,7 @@ function SearchStack() {
 function CreateDrinkStack() {
   return (
     <CreateDrinkNav.Navigator>
-      <CreateDrinkNav.Screen name="CreateDrink" component={CreateDrink} options={{ headerLeft: () => null }} />
+      <CreateDrinkNav.Screen name="CreateDrink" component={CreateDrink} options={{ ...headerOptions, headerLeft: () => null }} />
     </CreateDrinkNav.Navigator>
   );
 }
@@ -49,21 +61,21 @@ function CreateDrinkStack() {
 function OnTapStack() {
   return (
     <OnTapNav.Navigator>
-      <OnTapNav.Screen name="OnTap" component={OnTap} options={{ headerLeft: () => null }} />
-      <OnTapNav.Screen name="EditOnHand" component={EditOnHand} />
-      <OnTapNav.Screen name="DrinkDetail" component={DrinkDetail} />
+      <OnTapNav.Screen name="OnTap" component={OnTap} options={{ ...headerOptions, headerLeft: () => null }} />
+      <OnTapNav.Screen name="EditOnHand" component={EditOnHand} options={headerOptions} />
+      <OnTapNav.Screen name="DrinkDetail" component={DrinkDetail} options={headerOptions} />
     </OnTapNav.Navigator>
   );
 }
 
 function ProfileStack() {
-  const { colors } = useTheme();
   return (
     <ProfileNav.Navigator>
       <ProfileNav.Screen
         name="Profile"
         component={Profile}
         options={({ navigation }) => ({
+          ...headerOptions,
           headerLeft: () => null,
           headerRight: () => (
             <Spacer x amount={24}>
@@ -72,8 +84,8 @@ function ProfileStack() {
           ),
         })}
       />
-      <ProfileNav.Screen name="ProfileSettings" component={ProfileSettings} />
-      <ProfileNav.Screen name="DrinkDetail" component={DrinkDetail} />
+      <ProfileNav.Screen name="ProfileSettings" component={ProfileSettings} options={headerOptions} />
+      <ProfileNav.Screen name="DrinkDetail" component={DrinkDetail} options={headerOptions} />
     </ProfileNav.Navigator>
   );
 }
