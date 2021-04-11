efa81bb4d0e5aa4375a7fa5ecace383eb4e237c0
alexbmoreira <alexbmoreira@gmail.com>
2021-04-10 23:35:59 -0400

    Set up most of drink setail page
    Shows drink info as well as if it's liked and if it's on tap

--- frontend/src/pages/DrinkDetail.js
+++ frontend/src/pages/DrinkDetail.js
@@ -1,33 +1,92 @@
 import React, { useContext, useEffect } from 'react';
+import { StyleSheet } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
-import { Text, Title } from 'react-native-paper';
+import { Card, Text, Title } from 'react-native-paper';
+import { useTheme } from 'react-native-paper';
+import { FontAwesome } from '@expo/vector-icons';
+import BarCartIcons from '../../assets/fonts/BarCartIcons';
 
+import Flex from '../components/theme/Flex';
 import Spacer from '../components/theme/Spacer';
 
+import { Context as DrinkContext } from '../contexts/DrinkContext';
 import { Context as DrinkDetailContext } from '../contexts/DrinkDetailContext';
 
 function Home({ route, navigation }) {
+  const { colors } = useTheme();
+
   const { drinkID } = route.params;
-  const { state, getDrink } = useContext(DrinkDetailContext);
+  const { state: drinkState } = useContext(DrinkContext);
+  const { state: detailState, getDrink } = useContext(DrinkDetailContext);
 
   useEffect(() => {
     const ud = navigation.addListener('focus', async () => {
-      if (!state.drink || state.drink.id !== drinkID) {
+      if (!detailState.drink || detailState.drink.id !== drinkID) {
         await getDrink(drinkID);
       }
     });
     return ud;
-  }, [navigation, drinkID, getDrink, state]);
+  }, [navigation, drinkID, getDrink, detailState]);
+
+  const drinkIngredients = detailState.drink?.ingredients.map((ing, i) => {
+    return (
+      <Text key={ing.ingredient}>
+        • {ing.name}
+        {detailState.drink?.ingredients[i + 1] ? '\n' : null}
+      </Text>
+    );
+  });
+
+  const onTapDrink = () => {
+    const liked = drinkState.userOnTap.some((drink) => drink.id === detailState.drink?.id);
+    return liked ? <BarCartIcons name="on-tap" size={32} /> : null;
+  };
+
+  const likedDrink = () => {
+    const liked = drinkState.userLikes.some((drink) => drink.id === detailState.drink?.id);
+    return <FontAwesome name={liked ? 'heart' : 'heart-o'} size={32} />;
+  };
 
   return (
     <ScrollView>
       <Spacer>
-        <Title>Drink detail</Title>
-        <Text>{state.drink?.name}</Text>
-        <Text>{drinkID}</Text>
+        <Card>
+          <Card.Cover resizeMode="stretch" source={require('../../assets/drink_default.png')} />
+          <Card.Content>
+            <Title style={{ ...styles.alignText, color: colors.background }} numberOfLines={1}>
+              {detailState.drink?.name}
+            </Title>
+          </Card.Content>
+        </Card>
+        <Spacer y>
+          <Flex row>
+            {onTapDrink()}
+            {likedDrink()}
+          </Flex>
+        </Spacer>
+        <Title>Ingredients:</Title>
+        <Text>{drinkIngredients}</Text>
+        <Spacer y amout={7} />
+        <Title>Instructions:</Title>
+        <Text>{detailState.drink?.instructions}</Text>
       </Spacer>
     </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  card: {
+    width: 150,
+    height: 150,
+  },
+  cardCover: {
+    width: 150,
+    height: 110,
+  },
+  alignText: {
+    textAlign: 'center',
+    textAlignVertical: 'center',
+  },
+});
 
 export default Home;
